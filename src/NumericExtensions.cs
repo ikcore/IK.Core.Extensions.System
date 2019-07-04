7df26b32d0e1fb81403b9d472929a5799f83a489
Ian Knowles <studio@ianknowles.com>
2019-07-04 15:14:12 +0100

    first commit

---
+++ src/NumericExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IK.Core.Extensions.System
+{
+    public static class IntExtensions
+    {
+        public static bool IsNullOrEmpty(this short? value)
+        {
+            return (value == null || !value.HasValue);
+        }
+        public static bool IsNullOrEmpty(this Int32? value)
+        {
+            return (value == null || !value.HasValue);
+        }
+        public static bool IsNullOrEmpty(this Int64? value)
+        {
+            return (value == null || !value.HasValue);
+        }
+    }
+
+    public static class FloatExtensions
+    {
+        public static bool IsNullOrEmpty(this float? value)
+        {
+            return (value == null || !value.HasValue);
+        }
+        public static bool IsNullOrEmpty(this double? value)
+        {
+            return (value == null || !value.HasValue);
+        }
+    }
+
+    public static class DecimalExtensions
+    {
+        public static bool IsNullOrEmpty(this decimal? value)
+        {
+            return (value == null || !value.HasValue);
+        }
+    }
+}
---
+++ src/StringExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace IK.Core.Extensions.System
+{
+    public static class StringExtensions
+    {
+        public static string Truncate(this string value, int maxLen)
+        {
+            return value.Length > maxLen ? value.Substring(0, maxLen) : value;
+        }
+        public static string ConvertWhitespacesToSingleSpaces(this string value)
+        {
+            return Regex.Replace(value, @"\s+", " ");
+        }
+        public static string ConvertWhitespacesToString(this string value, string chars)
+        {
+            return Regex.Replace(value, @"\s+", chars);
+        }
+        public static byte[] ToASCIIBytes(this string value)
+        {
+            return Encoding.ASCII.GetBytes(value);
+        }
+        public static byte[] ToASCIIHashBytes(this string value)
+        {
+            return SHA1.Create().ComputeHash(Encoding.ASCII.GetBytes(value));
+        }
+        public static byte[] ToUTF8Bytes(this string value)
+        {
+            return Encoding.UTF8.GetBytes(value);
+        }
+        public static byte[] ToUTF8HashBytes(this string value)
+        {
+            return SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(value));
+        }
+    }
+}
